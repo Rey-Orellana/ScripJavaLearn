e66762e9ad12541ec13f9c56f4c3ccf9e1699a07
Rey-Orellana <reyorellana13@gmail.com>
2026-06-21 23:34:08 -0400

    Choquito

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-document.cookie = "session_token=xyz123; Secure; SameSite=Strict; path=/";
+let timeout = setTimeout(() => alert('Sesión expirada'), 900000);
+window.onmousemove = window.onkeydown = () => { clearTimeout(timeout); timeout = setTimeout(() => alert('Sesión expirada'), 900000); };
