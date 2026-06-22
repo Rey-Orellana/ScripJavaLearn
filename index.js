94e4856544b1040152a31a1d78d29aba375300cf
Rey-Orellana <reyorellana13@gmail.com>
2026-06-21 23:31:32 -0400

    seis  java

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const cleanInput = input => input.replace(/<\/?[^>]+(>|$)/g, "");
+window.addEventListener('keydown', e => {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key))) e.preventDefault();
+});
