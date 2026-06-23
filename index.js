9cac5992a26ddab754bfd494e74fb5c3799946d2
Rey-Orellana <reyorellana13@gmail.com>
2026-06-22 23:36:36 -0400

    Logger

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(lista.length); // 3 (Mantiene propiedades nativas)
+class Logger {
+  log(msg) { console.log(` LOG: ${msg}`); }
+}
+
+class ServicioUsuarios {
+  constructor(logger) {
+    this.logger = logger; // El logger es inyectado externamente
+  }
+
+  crear() {
+    this.logger.log("Usuario registrado con éxito");
+  }
+}
+
+const miLogger = new Logger();
+const servicio = new ServicioUsuarios(miLogger);
+servicio.crear();
