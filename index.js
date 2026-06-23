298a8cebde0b80c3fc1fbdb3c2592c4d3046a28d
Rey-Orellana <reyorellana13@gmail.com>
2026-06-22 23:33:54 -0400

    clases abstractuas

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-class Usuario {
-  #edad;
-
-  set edad(valor) {
-    if (valor < 0) throw new Error("La edad no puede ser negativa");
-    this.#edad = valor;
+class BaseDeDatos {
+  constructor() {
+    if (this.constructor === BaseDeDatos) {
+      throw new Error("No puedes instanciar una clase abstracta directamente.");
+    }
   }
 
-  get edad() {
-    return this.#edad;
+  conectar() {
+    throw new Error("El método 'conectar()' debe ser implementado.");
   }
 }
 
-const user = new Usuario();
-user.edad = 30; // Funciona
-// user.edad = -5; // Lanza Error
+// const db = new BaseDeDatos(); // Error
+class MySQL extends BaseDeDatos {}
+// new MySQL().conectar(); // Error: El método debe ser implementado
