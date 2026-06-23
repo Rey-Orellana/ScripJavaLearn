b7c99d127ea25d3a1ebbac330bf0523ab3224adc
Rey-Orellana <reyorellana13@gmail.com>
2026-06-22 23:34:50 -0400

    composicion sobre herencia

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-class Auto {
+class Motor {
+  encender() { return "Motor encendido..."; }
+}
+
+class Automovil {
   constructor() {
-    this.velocidad = 0;
+    this.motor = new Motor(); // Composición
   }
 
-  acelerar() {
-    this.velocidad += 10;
-    return this; // Clave para el encadenamiento
-  }
-
-  frenar() {
-    this.velocidad -= 5;
-    return this;
+  arrancar() {
+    return this.motor.encender() + " ¡Vámonos!";
   }
 }
 
-const miAuto = new Auto();
-miAuto.acelerar().acelerar().frenar();
-console.log(miAuto.velocidad); // 15
+const coche = new Automovil();
+console.log(coche.arrancar());
