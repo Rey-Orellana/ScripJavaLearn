7db396de8fc51f399e60343600ff46eb866601e2
Rey-Orellana <reyorellana13@gmail.com>
2026-06-22 23:36:25 -0400

    Builts ins

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-class Coleccion {
-  constructor() {
-    this.items = ["Item A", "Item B", "Item C"];
-  }
-
-  *[Symbol.iterator]() { // Generador para simplificar el iterador
-    for (let item of this.items) {
-      yield item;
-    }
-  }
-}
-
-const miLista = new Coleccion();
-for (let elemento of miLista) {
-  console.log(elemento); // Imprime A, B y C individualmente
-}
+console.log(lista.length); // 3 (Mantiene propiedades nativas)
