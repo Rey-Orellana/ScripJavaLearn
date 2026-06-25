8e29a3658794e3a44e1d15bc08c34df1a58b2752
Rey-Orellana <reyorellana13@gmail.com>
2026-06-24 22:56:03 -0400

    Busqueda y Filtrado anvazo

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-let videojuegos = [];
+const usuarios = [
+    { id: 1, nombre: 'Ana', rol: 'admin' },
+    { id: 2, nombre: 'Carlos', rol: 'user' },
+    { id: 3, nombre: 'Beatriz', rol: 'user' }
+];
 
-// C - Create
-function agregarJuego(id, titulo, genero) {
-    videojuegos.push({ id, titulo, genero });
-}
-// R - Read
-function listarJuegos() {
-    console.log('--- Lista de Juegos ---', videojuegos);
-}
-// U - Update
-function actualizarGenero(id, nuevoGenero) {
-    let juego = videojuegos.find(j => j.id === id);
-    if (juego) juego.genero = nuevoGenero;
-}
-// D - Delete
-function eliminarJuego(id) {
-    videojuegos = videojuegos.filter(j => j.id !== id);
-}
+// Leer un usuario por ID
+const usuarioId2 = usuarios.find(u => u.id === 2);
+console.log('Usuario con ID 2:', usuarioId2);
 
-// Prueba
-agregarJuego(1, 'Zelda', 'Aventura');
-agregarJuego(2, 'Doom', 'Shooter');
-actualizarGenero(1, 'RPG de Aventura');
-eliminarJuego(2);
-listarJuegos();
+// Filtrar usuarios por rol (Read avanzado)
+const usuariosNormales = usuarios.filter(u => u.rol === 'user');
+console.log('Usuarios con rol "user":', usuariosNormales);
