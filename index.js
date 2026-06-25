24c4b70c5046519a2bb3daff674e7c1085ce9f33
Rey-Orellana <reyorellana13@gmail.com>
2026-06-24 22:54:12 -0400

    manipulacion de arrays

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("easy")
+// 1. CREATE (Crear)
+let tareas = ['Aprender JS'];
+tareas.push('Practicar CRUD'); 
+console.log('Después de Create:', tareas);
+
+// 2. READ (Leer)
+console.log('Leyendo primera tarea:', tareas[0]);
+
+// 3. UPDATE (Actualizar)
+tareas[0] = 'Masterizar JS';
+console.log('Después de Update:', tareas);
+
+// 4. DELETE (Eliminar)
+tareas.splice(1, 1); // Elimina 1 elemento desde la posición 1
+console.log('Después de Delete:', tareas);
