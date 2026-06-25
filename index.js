748ce72fc71c2e8c1ff165191ef9239f62bb69b6
Rey-Orellana <reyorellana13@gmail.com>
2026-06-24 23:03:49 -0400

    Evitar Contaminacion de Parametros de HTTPS

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const jwt = require('jsonwebtoken');
+const hpp = require('hpp');
+const express = require('express');
+const app = express();
 
-function verificarToken(req, res, next) {
-    const token = req.header('Authorization')?.split(' ')[1];
-    if (!token) return res.status(401).send('Acceso denegado');
-
-    try {
-        const verificado = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verificado; // Añade los datos del usuario al request
-        next();
-    } catch (error) {
-        res.status(400).send('Token inválido o expirado');
-    }
-}
+app.use(express.urlencoded({ extended: true }));
+// Si detecta parámetros repetidos, los ignora y se queda solo con el último
+app.use(hpp());
