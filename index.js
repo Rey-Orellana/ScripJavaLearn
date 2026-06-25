75efb0bca7a9efb8d5711c2ef29408a087acc1e1
Rey-Orellana <reyorellana13@gmail.com>
2026-06-24 23:03:09 -0400

    Verificacion Segura de Tokesn JWT

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-app.use((req, res, next) => {
-    res.setHeader(
-        "Content-Security-Policy",
-        "default-src 'self'; script-src 'self' https://apis.google.com;"
-    );
-    next();
-});
+const jwt = require('jsonwebtoken');
+
+function verificarToken(req, res, next) {
+    const token = req.header('Authorization')?.split(' ')[1];
+    if (!token) return res.status(401).send('Acceso denegado');
+
+    try {
+        const verificado = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verificado; // Añade los datos del usuario al request
+        next();
+    } catch (error) {
+        res.status(400).send('Token inválido o expirado');
+    }
+}
