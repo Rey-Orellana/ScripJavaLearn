09726c3acbfe9c4b6e7aafe935936d3e8acecdf1
Rey-Orellana <reyorellana13@gmail.com>
2026-06-24 23:02:11 -0400

    Deshabilitar la cabecera

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const Joi = require('joi');
+const express = require('express');
+const app = express();
 
-const esquemaRegistro = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
-    email: Joi.string().email().required()
-});
-
-// Uso en middleware
-const { error, value } = esquemaRegistro.validate(req.body);
-if (error) {
-    return res.status(400).send('Datos de entrada inválidos');
-}
+// Elimina la cabecera X-Powered-By: Express
+app.disable('x-powered-by');
