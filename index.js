c7424dbb2134e0fa2b89ff0b9845fb2a8ffe4d67
Rey-Orellana <reyorellana13@gmail.com>
2026-07-23 23:03:57 -0400

    Metodos Estaticos

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-class Figura {
-    calcularArea() {
-        throw new Error("Debe implementar calcularArea()");
+class Calculadora {
+
+    static sumar(a, b) {
+        return a + b;
+    }
+
+    static restar(a, b) {
+        return a - b;
     }
 }
 
-class Rectangulo extends Figura {
-    constructor(base, altura) {
-        super();
-        this.base = base;
-        this.altura = altura;
-    }
-
-    calcularArea() {
-        return this.base * this.altura;
-    }
-}
-
-const rect = new Rectangulo(5, 4);
-console.log(rect.calcularArea());
+console.log(Calculadora.sumar(8, 2));
